fc9594d37dbfd7af0a5b2bf7ce09f309be7c7916
Yvette Hernández <yvhalo@gmail.com>
2022-06-09 13:21:12 +0200

    updates version_and_device_info fixing nulls when accessing parent milestones and runs

--- version_and_device_info.js
+++ version_and_device_info.js
@@ -1,6 +1,5 @@
 name: Version and device info
-description: If you create milestones as versions y testruns as devices, copies this information to result/fail views.
-I also use some custom fields
+description: Adds contextual milestone and test run info
 author: Yvette Hernández
 version: 1.0
 includes: ^(runs|tests)/view
@@ -10,20 +9,22 @@ js:
 $(document).ready(
     function() {
        $.subscribe('result_dialog.loaded', '', function(o) {
-          $('#addResultComment_display').html(
-           "version: " + uiscripts.context.milestone.name + "\n" +
-           "device: " + uiscripts.context.run.name
-          );
-          $('#addResultVersion').val(
-           uiscripts.context.milestone.name
-          );
-          var info = uiscripts.context.run.name.split("-");
-          $('#custom_device').val(
-           info[0].trim()
-          );
-          $('#custom_os_version').val(
-           info[1].trim()
-          );
+          if (uiscripts.context.milestone != null && uiscripts.context.run != null) {
+	          $('#addResultComment_display').html(
+	           "version: " + uiscripts.context.milestone.name + "\n" +
+	           "device: " + uiscripts.context.run.name
+	          );
+	          $('#addResultVersion').val(
+	           uiscripts.context.milestone.name
+	          );
+	          var info = uiscripts.context.run.name.split("-");
+	          $('#custom_device').val(
+	           info[0].trim()
+	          );
+	          $('#custom_os_version').val(
+	           info[1].trim()
+	          );
+	  }
        });
     }
 );
